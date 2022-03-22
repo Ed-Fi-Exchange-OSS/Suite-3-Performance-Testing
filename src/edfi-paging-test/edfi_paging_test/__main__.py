edad73d0149ec68ecf726241f1ef83fbda447a23
Stephen A. Fuqua <stephen@safnet.com>
2022-03-22 15:03:15 -0500

    Add global error handler

--- src/edfi-paging-test/edfi_paging_test/__main__.py
+++ src/edfi-paging-test/edfi_paging_test/__main__.py
@@ -3,15 +3,28 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+import sys
+
 from dotenv import load_dotenv
+from errorhandler import ErrorHandler
+
 from edfi_paging_test.helpers.argparser import parse_main_arguments
 from edfi_paging_test.performance_tester import run
 
 
 def main() -> None:
+    error_tracker = ErrorHandler()
 
     load_dotenv()
     run(parse_main_arguments())
+
+    if error_tracker.fired:
+        print(
+            "A fatal error occurred, please review the log output for more information.",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
