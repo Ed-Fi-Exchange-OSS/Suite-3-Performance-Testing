1c608f2dc2c0be86f838c72f33560b748ae577ac
vinaya <vinaya.mayya@ed-fi.org>
2022-04-01 15:54:43 -0500

    Added PowerShell automation

--- run-tests.bat
+++ run-tests.bat
@@ -2,16 +2,6 @@
 
 echo "Running tests from %CD%"
 
-REM Activate the EDFI_PERFORMANCE virtual environment for the remainder of this batch script.
-call C:\virtualenv\EDFI_PERFORMANCE\Scripts\activate.bat
-
-REM Install test suite dependencies.
-SET PYTHONWARNINGS=ignore:DEPRECATION
-python -m pip install --upgrade pip
-pip3 install -r requirements.txt
-
 REM Launch the test run.
 powershell -NoProfile -ExecutionPolicy Bypass -Command ". .\TestRunner.ps1; Invoke-%1Tests"
 
-REM Deactivate the EDFI_PERFORMANCE virtual environment.
-call deactivate
