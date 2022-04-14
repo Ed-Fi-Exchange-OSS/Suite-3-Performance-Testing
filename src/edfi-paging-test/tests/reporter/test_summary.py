6da233d2bf1f9bbd362fcdb695f9f91c1c71739a
vinaya <vinaya.mayya@ed-fi.org>
2022-04-14 15:44:57 -0500

    Added run configuration to summary report

--- src/edfi-paging-test/tests/reporter/test_summary.py
+++ src/edfi-paging-test/tests/reporter/test_summary.py
@@ -3,8 +3,11 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+from edfi_paging_test.helpers.log_level import LogLevel
+from edfi_paging_test.helpers.output_format import OutputFormat
 import pytest
 from edfi_paging_test.reporter.summary import Summary
+from edfi_paging_test.helpers.main_arguments import MainArguments
 import socket
 
 
@@ -15,53 +18,73 @@ def describe_when_initializing_summary() -> None:
         @pytest.fixture()
         def summary() -> Summary:
             return Summary(
-                key="123",
-                description="test",
-                resources=["resource1", "resource2"],
-                machine_name="m1"
+                run_name="123",
+                machine_name="m1",
+                run_configration=MainArguments(
+                    "http://api.ed-fi.org/v5.1",
+                    4,
+                    "populatedTemplateX",
+                    "populatedSecretX",
+                    "test_outputX",
+                    "test",
+                    OutputFormat.CSV,
+                    ["resource1", "resource2"],
+                    100,
+                    LogLevel.DEBUG,
+                )
             )
 
-        def it_sets_key(summary: Summary) -> None:
-            assert summary.key == "123"
+        def it_sets_run_name(summary: Summary) -> None:
+            assert summary.run_name == "123"
 
         def it_sets_description(summary: Summary) -> None:
-            assert summary.description == "test"
+            assert summary.run_configration.description == "test"
 
         def it_sets_machine_name(summary: Summary) -> None:
             assert summary.machine_name == "m1"
 
         def it_sets_resources(summary: Summary) -> None:
-            assert summary.resources == ["resource1", "resource2"]
+            assert summary.run_configration.resourceList == ["resource1", "resource2"]
 
     def describe_when_resource_not_provided() -> None:
         @pytest.fixture()
         def summary_without_resources() -> Summary:
             return Summary(
-                key="123",
-                description="test",
+                run_name="123",
+                run_configration=MainArguments(
+                    "http://api.ed-fi.org/v5.1",
+                    4,
+                    "populatedTemplateX",
+                    "populatedSecretX",
+                    "test_outputX",
+                    "test",
+                    OutputFormat.CSV,
+                    [],
+                    100,
+                    LogLevel.DEBUG,
+                )
             )
 
         def it_sets_default_value_for_resources(summary_without_resources: Summary) -> None:
-            assert summary_without_resources.resources == DEFAULT_RESOURCE_LIST
-
-    def describe_when_resource_is_None() -> None:
-        @pytest.fixture()
-        def summary_with_null_resources() -> Summary:
-            return Summary(
-                key="123",
-                description="test",
-                resources=[]
-            )
-
-        def it_sets_default_value_for_resources(summary_with_null_resources: Summary) -> None:
-            assert summary_with_null_resources.resources == DEFAULT_RESOURCE_LIST
+            assert summary_without_resources.run_configration.resourceList == DEFAULT_RESOURCE_LIST
 
     def describe_when_machine_name_not_provided() -> None:
         @pytest.fixture()
         def summary_without_host() -> Summary:
             return Summary(
-                key="123",
-                description="test",
+                run_name="123",
+                run_configration=MainArguments(
+                    "http://api.ed-fi.org/v5.1",
+                    4,
+                    "populatedTemplateX",
+                    "populatedSecretX",
+                    "test_outputX",
+                    "test",
+                    OutputFormat.CSV,
+                    ["resource1", "resource2"],
+                    100,
+                    LogLevel.DEBUG,
+                )
             )
 
         def it_sets_system_name(summary_without_host: Summary) -> None:
