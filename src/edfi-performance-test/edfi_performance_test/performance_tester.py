cee71bf0ac652ab09007ce7407f3344e9ad0b8bf
Stephen A. Fuqua <stephen@safnet.com>
2022-07-08 11:15:03 -0500

    Write CSV output

--- src/edfi-performance-test/edfi_performance_test/performance_tester.py
+++ src/edfi-performance-test/edfi_performance_test/performance_tester.py
@@ -6,16 +6,23 @@
 import logging
 import time
 import gevent
+from os import makedirs
 
 # from locust.debug import run_single_user # for running tests in a debugger
 from locust.env import Environment
-from locust.stats import stats_printer, stats_history
+from locust.stats import stats_history, StatsCSVFileWriter, PERCENTILES_TO_REPORT
 
 from edfi_performance_test.helpers.main_arguments import MainArguments
 from edfi_performance_test.helpers.config import set_config_values
 from edfi_performance_test.tasks.pipeclean.pipeclean_tests import DummyUser
 
 logger = logging.getLogger(__name__)
+
+
+def prepare_for_output(args: MainArguments) -> str:
+    makedirs(args.output, exist_ok=True)
+    output_base_path = f"{args.output}/pipeclean"
+    return output_base_path
 
 
 async def run(args: MainArguments) -> None:
@@ -34,7 +41,11 @@ async def run(args: MainArguments) -> None:
         env.create_web_ui("127.0.0.1", 8089)
 
         # start a greenlet that periodically outputs the current stats
-        gevent.spawn(stats_printer(env.stats))
+
+        stats_writer = StatsCSVFileWriter(
+            env, PERCENTILES_TO_REPORT, prepare_for_output(args), full_history=True
+        )
+        gevent.spawn(stats_writer)
 
         # start a greenlet that save current stats to history
         gevent.spawn(stats_history, env.runner)
@@ -46,8 +57,10 @@ async def run(args: MainArguments) -> None:
 
         runner.start(args.clientCount, spawn_rate=args.spawnRate)
 
-        # in 60 seconds stop the runner
-        gevent.spawn_later(args.runTimeInMinutes*60, lambda: runner.quit())  # stop the runner
+        # Manually stop locust, which otherwise would run continuously
+        gevent.spawn_later(
+            args.runTimeInMinutes * 60, lambda: runner.quit()
+        )
 
         # wait for the greenlets
         env.runner.greenlet.join()
