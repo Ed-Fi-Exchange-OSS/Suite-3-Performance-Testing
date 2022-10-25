739b07791555f4e2dbbe9086eec1ffc062f54e92
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-10-25 13:00:06 -0500

    add RDP rule to sql nsg

--- eng/terraform/modules/network/main.tf
+++ eng/terraform/modules/network/main.tf
@@ -27,6 +27,19 @@ resource "azurerm_network_security_group" "sql_sg" {
   location            = var.location
   resource_group_name = var.resource_group_name
 }
+resource "azurerm_network_security_rule" "rdp_sql_rule" {
+  name                        = "RDPRule"
+  resource_group_name         = var.resource_group_name
+  priority                    = 1200
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = 3389
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.sql_sg.name
+}
 resource "azurerm_network_security_rule" "sql_rule" {
   name                        = "MSSQLRule"
   resource_group_name         = var.resource_group_name
@@ -40,6 +53,7 @@ resource "azurerm_network_security_rule" "sql_rule" {
   destination_address_prefix  = "*"
   network_security_group_name = azurerm_network_security_group.sql_sg.name
 }
+
 resource "azurerm_virtual_network" "base_vnet" {
   name                = var.vnet_name
   location            = var.location
