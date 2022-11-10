537a9b3b13b7d442c76e994147277490d32efbcf
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-11-10 10:36:17 -0600

    fix formatting

--- eng/terraform/main.tf
+++ eng/terraform/main.tf
@@ -180,6 +180,6 @@ resource "azurerm_storage_account" "tf_state" {
   account_replication_type = "LRS"
 }
 resource "azurerm_storage_container" "tf_state" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tf_state.name
+  name                 = "tfstate"
+  storage_account_name = azurerm_storage_account.tf_state.name
 }
