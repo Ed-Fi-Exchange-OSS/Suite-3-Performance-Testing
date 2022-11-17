01eb3ebb8cc28e7f636ff6cbfc7d6c0b6b4856a1
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-11-17 14:48:47 -0600

    remove commented line

--- eng/terraform/modules/vm_scripts/main.tf
+++ eng/terraform/modules/vm_scripts/main.tf
@@ -9,7 +9,6 @@ resource "azurerm_virtual_machine_extension" "script_install" {
   type                 = "CustomScriptExtension"
   type_handler_version = "1.9"
   failure_suppression_enabled = false
-  #type_handler_version = "1.10"
   settings = <<SETTINGS
  {
   "commandToExecute": "powershell -command \"[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('${base64encode(data.template_file.script_file.rendered)}')) | Out-File -filepath ${var.script_filename}\" && powershell -ExecutionPolicy Unrestricted -File ${var.script_filename}"
