f7d383e18ed66d1f537bb09f24df32572ce87c88
Johnny Brenes <103782746+JBrenesSimpat@users.noreply.github.com>
2023-05-25 12:27:40 -0600

    [PERF-290] Add tests for better coverage of certification endpoints (#69)
    * Missing processes are created for Volume testing
    
    * Correction of class name StudentMigrantEducationProgramAssociationVolumeTest
    
    * Back to the original Year
    
    * Updating data from VomuleTest scenarios

---
+++ src/edfi-performance-test/edfi_performance_test/tasks/volume/session.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+from locust import task
+
+from edfi_performance_test.tasks.volume.ed_fi_volume_test_base import EdFiVolumeTestBase
+from edfi_performance_test.factories.utils import formatted_date
+
+
+class SessionVolumeTest(EdFiVolumeTestBase):
+    @task
+    def ru_session_scenarios(self):
+        self.run_scenario(
+            "endDate",
+            formatted_date(12, 10),
+            totalInstructionalDays=30,
+        )
