edfeef3480df8fbd427407ecc9df451f4a6127d6
Stephen A. Fuqua <stephen@safnet.com>
2023-06-22 12:29:10 -0500

    Remove unused import

--- src/edfi-performance-test/edfi_performance_test/tasks/pipeclean/composite.py
+++ src/edfi-performance-test/edfi_performance_test/tasks/pipeclean/composite.py
@@ -3,8 +3,6 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-from os import environ
-
 import traceback
 from greenlet import GreenletExit
 from typing import List
