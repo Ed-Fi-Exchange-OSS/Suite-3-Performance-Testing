d6a6b3cd1849facc8079bbf2fe97de45b2118c9e
JBrenesSimpat <johnny.brenes@simpat.tech>
2023-09-08 12:43:17 -0600

    new descriptors for v5

--- src/edfi-performance-test/edfi_performance_test/tasks/pipeclean/v5/descriptors.py
+++ src/edfi-performance-test/edfi_performance_test/tasks/pipeclean/v5/descriptors.py
@@ -5,6 +5,7 @@
 
 from edfi_performance_test.tasks.pipeclean.descriptors import (
     OldEthnicityPipecleanTest,
+    DescriptorPipecleanTestBase,
 )
 
 
@@ -12,3 +13,66 @@ class SkipOldEthnicityPipecleanTest(OldEthnicityPipecleanTest):
     @classmethod
     def skip_all_scenarios(cls):
         return True
+
+
+class EligibilityDelayReasonPipecleanTest(DescriptorPipecleanTestBase):
+    def __init__(self, parent, *args, **kwargs):
+        super(EligibilityDelayReasonPipecleanTest, self).__init__(
+            "eligibilityDelayReason", parent, *args, **kwargs
+        )
+
+
+class EligibilityEvaluationTypePipecleanTest(DescriptorPipecleanTestBase):
+    def __init__(self, parent, *args, **kwargs):
+        super(EligibilityEvaluationTypePipecleanTest, self).__init__(
+            "eligibilityEvaluationType", parent, *args, **kwargs
+        )
+
+
+class EnrollmentTypePipecleanTest(DescriptorPipecleanTestBase):
+    def __init__(self, parent, *args, **kwargs):
+        super(EnrollmentTypePipecleanTest, self).__init__(
+            "enrollmentType", parent, *args, **kwargs
+        )
+
+
+class EvaluationDelayReasonPipecleanTest(DescriptorPipecleanTestBase):
+    def __init__(self, parent, *args, **kwargs):
+        super(EvaluationDelayReasonPipecleanTest, self).__init__(
+            "evaluationDelayReason", parent, *args, **kwargs
+        )
+
+
+class IdeaPartPipecleanTest(DescriptorPipecleanTestBase):
+    def __init__(self, parent, *args, **kwargs):
+        super(IdeaPartPipecleanTest, self).__init__(
+            "ideaPart", parent, *args, **kwargs
+        )
+
+
+class ProgramEvaluationPeriodPipecleanTest(DescriptorPipecleanTestBase):
+    def __init__(self, parent, *args, **kwargs):
+        super(ProgramEvaluationPeriodPipecleanTest, self).__init__(
+            "programEvaluationPeriod", parent, *args, **kwargs
+        )
+
+
+class ProgramEvaluationTypePipecleanTest(DescriptorPipecleanTestBase):
+    def __init__(self, parent, *args, **kwargs):
+        super(ProgramEvaluationTypePipecleanTest, self).__init__(
+            "programEvaluationType", parent, *args, **kwargs
+        )
+
+
+class RatingLevelPipecleanTest(DescriptorPipecleanTestBase):
+    def __init__(self, parent, *args, **kwargs):
+        super(RatingLevelPipecleanTest, self).__init__(
+            "ratingLevel", parent, *args, **kwargs
+        )
+
+
+class SchoolChoiceBasisPipecleanTest(DescriptorPipecleanTestBase):
+    def __init__(self, parent, *args, **kwargs):
+        super(SchoolChoiceBasisPipecleanTest, self).__init__(
+            "schoolChoiceBasis", parent, *args, **kwargs
+        )
