e4c67f49b070a1656cafb219ff439f1b9b22adc3
JBrenesSimpat <johnny.brenes@simpat.tech>
2023-09-12 11:44:48 -0600

    install dependencies for reports

--- run-report.bat
+++ run-report.bat
@@ -9,6 +9,9 @@ REM Write path to test results folder for notebook to process.
 
 echo %2>notebook_input.txt
 
+REM Executing: poetry install
+poetry install
+
 REM Launch the notebook run.
 powershell -NoProfile -ExecutionPolicy Bypass -Command "poetry run jupyter nbconvert --output-dir='.\..\..\' --to html --execute '%1 Test Analysis.ipynb' --no-input" 2>&1
 
