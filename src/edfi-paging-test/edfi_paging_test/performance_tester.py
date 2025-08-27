bc53fb86c3b77d318ff6fc0e8dc47aa6e81e9258
Adam Hopkins <adam.hopkins@simpat.tech>
2025-08-27 11:53:48 -0500

    Revert max_workers

--- src/edfi-paging-test/edfi_paging_test/performance_tester.py
+++ src/edfi-paging-test/edfi_paging_test/performance_tester.py
@@ -86,7 +86,7 @@ async def run(args: MainArguments) -> None:
         request_client: RequestClient = RequestClient(args)
 
         executor: ThreadPoolExecutor = ThreadPoolExecutor(
-            max_workers=1  # args.connectionLimit TODO AXEL revert
+            max_workers=args.connectionLimit
         )
         loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 
